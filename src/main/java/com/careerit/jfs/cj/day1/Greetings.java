cb86cd70017c3c5b51926714f3209a69ac11dc8e
hariabhi96522 <areteharikareddy@gmail.com>
2023-04-21 14:45:06 +0530

    added extra log message and multiplication of tablel added

--- src/main/java/com/careerit/jfs/cj/day1/Greetings.java
+++ src/main/java/com/careerit/jfs/cj/day1/Greetings.java
@@ -1,5 +1,13 @@
 public class Greetings {
     public static void main(String[] args) {
+        String username = "user";
+        if(args.length > 0)
+        {
+            username = args[0];
+        }
+
         System.out.println("Hello world!");
+        System.out.println("hi "+username+",\n\tWelcome to java World");
     }
+
 }
---
+++ src/main/java/com/careerit/jfs/cj/day1/MultiplicationOfTable.java
@@ -0,0 +1,14 @@
+package com.careerit.jfs.cj.day1;
+
+    public class MultiplicationOfTable {
+
+        public static void main(String[] args) {
+
+            int num = 5;
+            for(int i = 1; i <= 10; ++i)
+            {
+                System.out.printf("%d * %d = %d \n", num, i, num * i);
+            }
+        }
+    }
+
